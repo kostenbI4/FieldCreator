bbd6a91397fd6682e245e56a043b038cfbae38de
Aleksandr Kostenko <avk076@gmail.com>
2022-07-22 12:44:32 +0300

    Добавил обработку мишиного описания

--- Main.py
+++ Main.py
@@ -26,18 +26,28 @@ def getFildByName(name, rusText):
 
 
 def getRussianString(line):
-    reg = r"[№а-яА-Я]+[а-яА-Я,. :-№]+"
+    reg = r"[№а-яА-Я]+[а-яА-Я,. :-№()\[\]]+"
     match = re.search(reg, line)
     rez = match[0] if match else 'Not found'
-    if getFildName(line)[:1]=="i":
+    if getFildName(line)[:1] == "i":
         rez = "ID " + rez
     return rez
 
 
 def getFildName(line):
-    reg = r"^[a-zA-Z_]+"
+    # удаляем Мишины: блаблабла.ХерМоржовый as бубликСраный
+    reg = r".+[\s\t]as[\s\t]"
     match = re.search(reg, line)
+    rez = ""
+    if match:
+        rez = line.replace(match[0], "")
+    else:
+        rez = line
+    reg = r"^[a-zA-Z_.]+"
+    match = re.search(reg, rez)
     rez = match[0] if match else 'Not found'
+    match = re.search(r"[a-zA-z]+\.", rez)
+    if match: rez = rez.replace(match[0], "")
     return rez
 
 
@@ -53,6 +63,5 @@ def getFilds(text):
     for i in lines:
         str += getFildByName(getFildName(i), getRussianString(i))
     return str
-
 
 # getFilds("""""")
